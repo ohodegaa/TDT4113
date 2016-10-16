ab4bb37aa2b40bc8dc0d8ceeb1319cdbd707aa6d
ohodegaa <ohodegaa@stud.ntnu.no>
2016-10-16 18:20:39 +0200

    oving_7 started

---
+++ Ovinger/Oving_4/file_handler.py
@@ -0,0 +1,6 @@
+__author__ = 'ohodegaa'
+
+class file_reader():
+
+    def __init__(self, dir_path = "/Users/olehakon95/Documents/OneDrive - NTNU/Arstrinn_2/semester_3_host/ProgLAB/Ovinger/Oving_4/data"):
+        dir_path
--- README.md
+++
@@ -1,2 +0,0 @@
-# ProgLAB
-# ProgLab
